4b4da9f75bc57071c5e593c74ac41a0206936baf
Rafsanjani Aziz <coded_raf@yahoo.com>
2023-06-22 12:17:25 +0100

    Add compose mpp dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "io.github.rafsanjani"
-version = "0.0.7"
+version = "0.0.8"
 
 plugins {
     `version-catalog`
@@ -69,7 +69,7 @@ publishing {
     repositories {
         maven {
             setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            version = "0.0.7"
+            version = "0.0.8"
             group = "io.github.rafsanjani"
 
             credentials {
