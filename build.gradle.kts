504beca661ec307de6f21724351a232e6301bd49
Rafsanjani Aziz <9197459+rafsanjani@users.noreply.github.com>
2023-10-12 17:50:49 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.gradle.internal.impldep.org.bouncycastle.cms.RecipientId.password
 
-val libraryVersion = "2023.10.07"
+val libraryVersion = "2023.10.12"
 val libraryGroup = "io.github.rafsanjani"
 
 group = libraryGroup
