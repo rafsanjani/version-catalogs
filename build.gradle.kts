229186017171d70c4f05d1b39fc1e0b6ab359ab4
rafsanjani <9197459+rafsanjani@users.noreply.github.com>
2024-12-26 10:41:14 +0000

    Bump android version code to 2024.12.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import kotlin.io.path.Path
 
-val libraryVersion = "2024.12.25"
+val libraryVersion = "2024.12.26"
 val libraryGroup = "io.github.rafsanjani"
 
 group = libraryGroup
