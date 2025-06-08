b8695dddad62d864e11e36b83b7b4bd5fb3abea9
Rafsanjani Aziz <9197459+rafsanjani@users.noreply.github.com>
2025-06-08 01:49:13 +0100

    Bump android version code to 2025.06.08 (#1099)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
-val libraryVersion = "2025.06.01"
+val libraryVersion = "2025.06.08"
 val libraryGroup = "io.github.rafsanjani"
 
 group = libraryGroup
