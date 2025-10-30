2bcf9d5cd8deed2f102f27646f72b39ec2a19550
Rafsanjani Aziz <coded_raf@yahoo.com>
2025-10-30 09:38:55 +0000

    Use targetsdk from libs.toml file

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.foreverrafs.sample"
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.foreverrafs.sample"
